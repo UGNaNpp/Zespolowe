1f7b526f6b9dc0aeb70df3ced415ed13e57c44d5
I-am-not-a-weeb <kubutek04@gmail.com>
2025-01-12 17:23:04 +0100

    main -> add import
    Signed-off-by: I-am-not-a-weeb <kubutek04@gmail.com>

--- server/src/main/java/org/server/ServerApplication.java
+++ server/src/main/java/org/server/ServerApplication.java
@@ -18,6 +18,7 @@ import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
