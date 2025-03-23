7ac386e05982175d952f6558deab23f6c4382db7
Mateusz Domino <mdomino60@gmail.com>
2025-03-23 02:00:31 +0100

    Add login page

--- web_app/src/app/login/page.tsx
+++ web_app/src/app/login/page.tsx
@@ -1,11 +1,19 @@
-import NavBar from '../components/navbar/NavBar';
+import styles from './loginStyle.module.scss';
 
 export default async function Login() {
+  const loginTitle = process.env.LOGIN_TITLE;
 
   return (
-    <div>
-      <NavBar title='Login' titleUrl='/login' subtitle='' subtitleUrl=''/>
-      <h1>Logowanie</h1>
+    <div className={styles.background}>
+      <div className={styles.mainForm}>
+        <h2>{loginTitle}</h2>
+        <div className={styles.buttonDiv}>
+          <button>
+            <i className="fa-brands fa-github"></i>
+            <span>Login with github</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
