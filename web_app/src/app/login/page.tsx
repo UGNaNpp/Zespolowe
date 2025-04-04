afbf7b37935b1bb5aaa52e655a9f4bef210ced09
Mateusz Domino <mdomino60@gmail.com>
2025-04-04 22:22:43 +0200

    Add authentication to front-end

--- web_app/src/app/login/page.tsx
+++ web_app/src/app/login/page.tsx
@@ -1,14 +1,18 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import styles from './loginStyle.module.scss';
 
-export default async function Login() {
-  const loginTitle = process.env.LOGIN_TITLE;
+export default function Login() {
+  const loginTitle = process.env.NEXT_PUBLIC_LOGIN_TITLE;
 
   return (
     <div className={styles.background}>
       <div className={styles.mainForm}>
         <h2>{loginTitle}</h2>
         <div className={styles.buttonDiv}>
-          <button>
+          <button onClick={() => signIn("github", { callbackUrl: "/", method: "POST" })}>
             <i className="fa-brands fa-github"></i>
             <span>Login with github</span>
           </button>
