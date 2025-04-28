2c05dfca85f88929cc571a3fd569fa41a4bb6d5e
kopcz <kopczewski.michal101@gmail.com>
2025-04-28 16:49:44 +0200

    Budowanie obrazu dockera + prosty docker compose dla serwera

--- server/src/main/java/org/server/ServerApplication.java
+++ server/src/main/java/org/server/ServerApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        String jwtSecret = dotenv.get("JWT_SECRET");
-        System.setProperty("JWT_SECRET", jwtSecret);
-        System.setProperty("TIME_ZONE", dotenv.get("TIME_ZONE"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-        System.setProperty("FRONT_URL", dotenv.get("FRONT_URL"));
+        // Odkomentować przy lokalnym odpalaniu programu z plikiem .env
+
+//        Dotenv dotenv = Dotenv.configure().load();
+//        String jwtSecret = dotenv.get("JWT_SECRET");
+//        System.setProperty("JWT_SECRET", jwtSecret);
+//        System.setProperty("TIME_ZONE", dotenv.get("TIME_ZONE"));
+//        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+//        System.setProperty("FRONT_URL", dotenv.get("FRONT_URL"));
         SpringApplication.run(ServerApplication.class, args);
     }
 
