365699322fa8b042ef0cf7d7128e666b9ebddac8
Mateusz Domino <mdomino60@gmail.com>
2025-05-03 20:06:28 +0200

    Add multi language support

--- web_app/src/app/[lang]/layout.tsx
+++ web_app/src/app/[lang]/layout.tsx
@@ -2,7 +2,7 @@
 
 // import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { SessionProvider } from "next-auth/react";
 
